fe344dfa7f9369c81cc7527f8354e35fa31e348f
gwleuverink <willem@leuver.ink>
2026-01-25 02:41:12 +0100

    fix testbench compat

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,8 @@ namespace Tests;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
+static $latestResponse = null;
+
 abstract class TestCase extends BaseTestCase
 {
     use WithWorkbench;
